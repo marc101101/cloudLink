8324a010e7a3e9187c19eadf16a7f87f19e24ef6
markus <markus@markus-EasyNote-TM85.(none)>
2014-03-03 16:00:54 +0100

    basic settings for login_form

---
+++ app/controllers/login_form_controller.rb
@@ -0,0 +1,2 @@
+class LoginFormController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 CloudLink::Application.routes.draw do
+  
+  
+  root :to => 'login_form#index'
+  
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
