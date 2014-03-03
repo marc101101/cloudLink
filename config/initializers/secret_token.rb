3b1afdf1b9f9fe814c89fa07a01dac574485e12e
markus <markus@markus-EasyNote-TM85.(none)>
2014-03-03 14:19:13 +0100

    first project version of cloudLink

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CloudLink::Application.config.secret_key_base = 'dee173ad98d81e3980b4babdbd994745a46765c39621a064ecf8f97e5827ee61db73900479068870f2a064361bbccf92ce41c3a42356ce1c07f07fd965c58ae0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CloudLink::Application.config.session_store :cookie_store, key: '_cloudLink_session'
